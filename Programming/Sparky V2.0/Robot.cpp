3dafd69500cedf8519c38dbc06035daf37386467
Brannon Penrod <43364611+brannon-penrod@users.noreply.github.com>
2019-02-16 17:10:26 -0800

    Update Robot.cpp

--- Programming/Sparky V2.0/Robot.cpp
+++ Programming/Sparky V2.0/Robot.cpp
@@ -10,6 +10,7 @@
 #include <frc/BuiltInAccelerometer.h>
 #include <cameraserver/CameraServer.h>
 #include <wpi/raw_ostream.h>
+#include <frc/DigitalInput.h>
 
 #define __linux__
 
@@ -30,19 +31,20 @@ double accelMult = accelMultMin; //used to modify acceleration multiplier for th
 const double accelMultMax = 0.75; //max for the acceleration multiplier
 
 Joystick joy_driver { 0 }; //controller for main driver
-int driver_leftStickY = 1, driver_rightStickY = 5; //driver sticks
+//axis and button values on the controller
+int driver_leftStickY = 1, driver_rightStickY = 5;
 
 // CO-DRIVER //
 WPI_TalonSRX tln_arm { 5 }; //arm connected to Cargo intake box
-const double armAccelMultMin = 0.1; //minimum value for the arm acceleration multiplier
-double armAccelMult = armAccelMultMin; //acceleration multiplier for the double bar arm on the robot
-const double armAccelMultMax = 0.25; //max for the arm acceleration multiplier
+const double armVelMult = 0.25;
+
+DigitalInput limiterSwitchBottom { 4 }, limiterSwitchTop { 5 };
 
 WPI_VictorSPX vic_cargoIO { 7 }; //axel + wheels used for intake and output of cargo balls
 
 Joystick joy_codriver { 1 }; //controller for co driver (Arm control)
+//axis and button values on the controller
 int co_stickY = 1, co_cargoPowerToggle = 2, co_cargoIOToggle = 3;
-bool cargoOn = false;
 
 // MISCELLANEOUS //
 const double pi = 3.1415926535897; //used for encoder distances
@@ -62,39 +64,34 @@ void Robot::RobotInit() {
   SmartDashboard::PutData("Auto Modes", &m_chooser);
 
   enc_rightWheel.SetSamplesToAverage(5); 
-  enc_rightWheel.SetDistancePerPulse(1.0 / 360.0 * 2.0 * pi * 3);
+  enc_rightWheel.SetDistancePerPulse(1.0 / 360.0 * 2.0 * pi * 3.0);
   enc_rightWheel.SetMinRate(1.0);
 
   enc_leftWheel.SetSamplesToAverage(5);
-  enc_leftWheel.SetDistancePerPulse(1.0 / 360.0 * 2.0 * pi * 3);
+  enc_leftWheel.SetDistancePerPulse(1.0 / 360.0 * 2.0 * pi * 3.0);
   enc_leftWheel.SetMinRate(1.0);
 
   tln_arm.SetNeutralMode(NeutralMode::Brake);
+  tln_arm.SetInverted(true);
+  
+  SmartDashboard::PutString("Cargo Intake Direction: ", (vic_cargoIO.GetInverted() ? "In" : "Out"));
 }
 
 void Robot::RobotPeriodic() {
 
-  bool sticksMoving = joy_driver.GetRawAxis(driver_leftStickY) > 0.1 || joy_driver.GetRawAxis(driver_rightStickY) > 0.1 || 
+  //checks if the sticks are pushed forward or backward
+  bool sticksMovingY = joy_driver.GetRawAxis(driver_leftStickY) > 0.1 || joy_driver.GetRawAxis(driver_rightStickY) > 0.1 || 
      joy_driver.GetRawAxis(driver_leftStickY) < -0.1 || joy_driver.GetRawAxis(driver_rightStickY) < -0.01;
 
-  if(sticksMoving && accelMult < accelMultMax) { //one of the sticks were moved
+  if(sticksMovingY && accelMult < accelMultMax) { //one of the sticks were moved
     accelMult += 0.01; //add 0.01 to the acceleration multiplier
-  } else if(sticksMoving && accelMult >= accelMultMax) { //the acceleration multiplier has reached its maximum value
+  } else if(sticksMovingY && accelMult >= accelMultMax) { //the acceleration multiplier has reached its maximum value
     accelMult = accelMultMax;
   } else { //the sticks aren't being pushed
     accelMult = accelMultMin; //reset the acceleration multiplier
   }
 
-  bool armStickMoving = joy_codriver.GetRawAxis(co_stickY) > 0.1 || joy_codriver.GetRawAxis(co_stickY) < -0.01;
-
-  if(armStickMoving && armAccelMult < armAccelMultMax){
-    armAccelMult += 0.01;
-  } else if (armStickMoving && armAccelMult >= armAccelMultMax) {
-    armAccelMult = armAccelMultMax;
-  } else { 
-    armAccelMult = 0.1;
-  }
-
+  SmartDashboard::UpdateValues();
 }
 
 void Robot::AutonomousInit() {
@@ -114,10 +111,12 @@ void Robot::AutonomousPeriodic() {
   if (m_autoSelected == kAutoNameCustom) {
     // Custom Auto goes here
   } else {
-    while(enc_leftWheel.GetDistance() < 10) {
+    while(enc_leftWheel.GetDistance() < 10) { //the left wheel's distance is less than 10
+      //set the drive to 0.5 speed
       drv_wheels.TankDrive(0.5, 0.5);
     }
-    
+
+    //stop the drive
     drv_wheels.TankDrive(0, 0);
   }
 
@@ -126,18 +125,36 @@ void Robot::AutonomousPeriodic() {
 void Robot::TeleopInit() {}
 
 void Robot::TeleopPeriodic() {
+  //tank drive that multiplies by the acceleration multiplier (determined in RobotPeriodic())
   drv_wheels.TankDrive(joy_driver.GetRawAxis(driver_leftStickY) * accelMult, joy_driver.GetRawAxis(driver_rightStickY) * accelMult);
-  //if (!(limiterSwitch.GetPressed()) {
-    tln_arm.Set(joy_codriver.GetRawAxis(co_stickY) * 0.5);
-  //}
 
-  if(joy_codriver.GetRawButton(co_cargoPowerToggle)) {
-    vic_cargoIO.Get() > 0.1 ? vic_cargoIO.Set(0.0) : vic_cargoIO.Set(0.5);
+  //neither limiter switch is hit
+  if (!limiterSwitchBottom.Get() && !limiterSwitchTop.Get()) {
+    //move the arm
+    tln_arm.Set(joy_codriver.GetRawAxis(co_stickY) * armVelMult);
+  } 
+  //the bottom limiter switch is hit and the joystick is pushed forward
+  else if(limiterSwitchBottom.Get() && joy_codriver.GetRawAxis(co_stickY) > 0) { 
+    //move the arm
+    tln_arm.Set(joy_codriver.GetRawAxis(co_stickY) * armVelMult);
+  } 
+  //the top limiter switch is hit and the joystick is pulled backward
+  else if(limiterSwitchTop.Get() && joy_codriver.GetRawAxis(co_stickY) < 0) {
+    //move the arm
+    tln_arm.Set(joy_codriver.GetRawAxis(co_stickY) * armVelMult);
   }
 
-  if(joy_codriver.GetRawButton(co_cargoIOToggle)){
+  if(joy_codriver.GetRawButtonPressed(co_cargoPowerToggle)) {
+    //set the cargo motor off if it's on, or on if it's off.
+    vic_cargoIO.Get() > 0.0 ? vic_cargoIO.Set(0.0) : vic_cargoIO.Set(0.5);
+  }
+
+  if(joy_codriver.GetRawButtonPressed(co_cargoIOToggle)) {
+    //set the cargo motor inverted if it's not, or not if it is.
     vic_cargoIO.GetInverted() ? vic_cargoIO.SetInverted(false) : vic_cargoIO.SetInverted(true);
   }
+
+  SmartDashboard::UpdateValues();
 }
 
 //Use these functions to test controls BEFORE putting them in periodic classes.

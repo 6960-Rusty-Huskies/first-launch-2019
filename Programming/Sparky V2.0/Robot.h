84321a4ff8d860a9e29d680b9b9e7b6f6019930b
Brannon Penrod <43364611+brannon-penrod@users.noreply.github.com>
2019-02-19 00:14:42 -0800

    Update Robot.h

--- Programming/Sparky V2.0/Robot.h
+++ Programming/Sparky V2.0/Robot.h
@@ -15,46 +15,33 @@ class Robot : public frc::TimedRobot {
  public:
   void RobotInit() override;
   void RobotPeriodic() override;
+
   void AutonomousInit() override;
   void AutonomousPeriodic() override;
+
   void TeleopInit() override;
   void TeleopPeriodic() override;
+
+  void DisabledInit() override;
+  void DisabledPeriodic() override;
+
   void TestInit() override;
   void TestPeriodic() override;
+
   static void VisionThread() {
     // Get the USB camera from CameraServer
     cs::UsbCamera camera = frc::CameraServer::GetInstance()->StartAutomaticCapture();
-    // Set the resolution
+    // Set the resolution and FPS
     camera.SetResolution(640, 480);
-
-    // Get a CvSink. This will capture Mats from the Camera
-    cs::CvSink cvSink = frc::CameraServer::GetInstance()->GetVideo();
-    // Setup a CvSource. This will send images back to the Dashboard
-    cs::CvSource outputStream = frc::CameraServer::GetInstance()->PutVideo("Rectangle", 640, 480);
-
-    // Mats are very memory expensive. Lets reuse this Mat.
-    cv::Mat mat;
-
-    while (true) {
-      // Tell the CvSink to grab a frame from the camera and put it
-      // in the source mat.  If there is an error notify the output.
-      if (cvSink.GrabFrame(mat) == 0) {
-        // Send the output the error.
-        outputStream.NotifyError(cvSink.GetError());
-        // skip the rest of the current iteration
-        continue;
-      }
-      // Put a rectangle on the image
-      rectangle(mat, cv::Point(100, 100), cv::Point(400, 400),
-                cv::Scalar(255, 255, 255), 5);
-      // Give the output stream a new image to display
-      outputStream.PutFrame(mat);
-    }
+    camera.SetFPS(30);
   };
+  
+  void UpdateDashboardValues();
 
  private:
   frc::SendableChooser<std::string> m_chooser;
   const std::string kAutoNameDefault = "Default";
   const std::string kAutoNameCustom = "My Auto";
   std::string m_autoSelected;
+  
 };

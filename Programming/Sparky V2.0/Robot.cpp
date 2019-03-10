2b8e42dfbedb7d3963d4288fdafb01f80ac26162
TheMarkVI <34019229+TheMarkVI@users.noreply.github.com>
2019-03-10 14:56:50 -0700

    Update Robot.cpp

--- Programming/Sparky V2.0/Robot.cpp
+++ Programming/Sparky V2.0/Robot.cpp
@@ -31,12 +31,14 @@ WPI_TalonSRX tln_leftBack { 2 };
 SpeedControllerGroup grp_left { tln_leftFront, tln_leftBack };
 
 // Left wheel encoder
-Encoder enc_leftWheel { 0, 1 };
+Encoder enc_leftWheel{ 0, 1 };
 
 // Right motors
 WPI_TalonSRX tln_rightFront { 3 };
 WPI_TalonSRX tln_rightBack { 4 };
 SpeedControllerGroup grp_right { tln_rightFront, tln_rightBack };
+
+double wheelVelMult = 0.75;
 
 // Right wheel encoder
 Encoder enc_rightWheel { 2, 3 };
@@ -46,18 +48,22 @@ DifferentialDrive drv_wheels { grp_left, grp_right };
 
 // Elevator solenoids
 DoubleSolenoid sol_frontActuators { 2, 3 },
-               sol_backActuators { 6, 7 };
+    sol_backActuators { 6, 7 };
 
 // Actuator wheel
-WPI_VictorSPX vic_actuatorBackWheel { 6 };
+WPI_VictorSPX vic_actuatorRight { 7 };
+WPI_VictorSPX vic_actuatorLeft { 8 };
+SpeedControllerGroup grp_act { vic_actuatorLeft, vic_actuatorRight };
+
+double actWheelVelMult = 0.75;
 
 // Controller for main driver
 Joystick joy_driver { 0 };
 // Axis and button values on the controller
 int drvr_leftStickY = 1, drvr_rightStickY = 3,
-    drvr_actWheelPower = 1, drvr_actWheelInvert = 2,
-    drvr_actsDown = 1, drvr_frontActUp = 2,
-    drvr_backActUp = 3;
+    drvr_actWheelForward = 1, drvr_actWheelBack = 2,
+    drvr_actsDown = 3, drvr_frontActUp = 4,
+    drvr_backActUp = 5;
 
 // CO-DRIVER //
 
@@ -66,7 +72,7 @@ WPI_TalonSRX tln_arm { 5 };
 // Multiplier for speed of the arm
 const double armVelMult = 0.75;
 
-DigitalInput limiterSwitchTop { 4 }, limiterSwitchBottom { 5 };
+DigitalInput limiterSwitchTop{4}, limiterSwitchBottom { 5 };
 
 //axel + wheels used for intake and output of cargo balls
 WPI_VictorSPX vic_cargoIO { 7 };
@@ -78,29 +84,31 @@ int co_stickY = 1, co_cargoPowerToggle = 1, co_cargoIOToggle = 2;
 
 // MISCELLANEOUS //
 
-//used for encoder distances
+// used for encoder distances
 const double pi = 3.1415926535897;
 
 Compressor compressor;
 
 // End variable declaration //
 
-void Robot::RobotInit() {
+void Robot::RobotInit()
+{
+  compressor.Stop();
   // We need to run our vision program in a separate thread. If not, our robot
   // will not run.
   std::thread visionThread(VisionThread);
   visionThread.detach();
 
-  // Sets the number of the samples the encoder gets to 
+  // Sets the number of the samples the encoder gets to
   // average when calculating period.
-  enc_rightWheel.SetSamplesToAverage(5); 
+  enc_rightWheel.SetSamplesToAverage(5);
 
-  // Used to calculate distance traveled in one rotation. 
-  // Distance in this case is calculated in inches per second. 
+  // Used to calculate distance traveled in one rotation.
+  // Distance in this case is calculated in inches per second.
   // Formula: 1.0 / 360.0 * 2.0 * pi * radius
   enc_rightWheel.SetDistancePerPulse(1.0 / 360.0 * 2.0 * pi * 3.0);
 
-  // Sets the minimum number of inches per second for 
+  // Sets the minimum number of inches per second for
   // the robot to be considered to be moving.
   enc_rightWheel.SetMinRate(1.0);
 
@@ -115,54 +123,55 @@ void Robot::RobotInit() {
   // Set the left wheel inverted so it matches with the right one.
   grp_left.SetInverted(true);
 
-  // Set the right back talon inverted so it doesn't move 
+  // Set the right back talon inverted so it doesn't move
   // in the opposite direction of the other one.
   tln_rightBack.SetInverted(true);
+  
+  SmartDashboard::SetDefaultNumber("Drivetrain Speed Multiplier: ", 0.75);
 }
 
-
-void Robot::RobotPeriodic() {
+void Robot::RobotPeriodic()
+{
   // Update the values on the Smart Dashboard.
   UpdateDashboardValues();
 }
 
-void Robot::AutonomousInit() {}
-
-void Robot::AutonomousPeriodic() {
-  // The left wheel's distance is less than the maximum
-  while(enc_leftWheel.GetDistance() < 10) {
-    // Set the drive to 0.5 speed
-    //drv_wheels.TankDrive(0.5, 0.5);
-  }
-    // Stop the drive
-    drv_wheels.TankDrive(0, 0);
+void Robot::AutonomousInit()
+{
+  Robot::TeleopInit();
 }
 
-void Robot::TeleopInit() {
-  /*
-  sol_backActuators.Set(DoubleSolenoid::Value::kReverse);
-  sol_frontActuators.Set(DoubleSolenoid::Value::kReverse);
-  */
+void Robot::AutonomousPeriodic()
+{
+  Robot::TeleopPeriodic();
 }
 
-void Robot::TeleopPeriodic() {
+void Robot::TeleopInit() {}
+
+void Robot::TeleopPeriodic()
+{
   // DRIVER CONTROLS //
-  drv_wheels.TankDrive(joy_driver.GetRawAxis(drvr_leftStickY), joy_driver.GetRawAxis(drvr_rightStickY) );
-  
-  // The button must be held to drive.
-  if(joy_driver.GetRawButtonPressed(drvr_actWheelPower)) {
-    vic_actuatorBackWheel.Set(0.25);
+  drv_wheels.TankDrive(wheelVelMult * joy_driver.GetRawAxis(drvr_leftStickY), wheelVelMult * joy_driver.GetRawAxis(drvr_rightStickY));
+
+  /*
+  // The POV must be held to drive.
+  // forward
+  if (joy_driver.GetPOV(0))
+  {
+    grp_act.Set(actWheelVelMult);
+  }
+  // back
+  if (joy_driver.GetPOV(180))
+  {
+    grp_act.Set(-actWheelVelMult);
   }
 
-  //Stop the wheels.
-  if(joy_driver.GetRawButtonReleased(drvr_actWheelPower)) {
-    vic_actuatorBackWheel.Set(0.0);
+  // The POV is not pressed
+  if (joy_driver.GetPOV(-1))
+  {
+    grp_act.Set(0.0);
   }
 
-  if(joy_driver.GetRawButtonPressed(drvr_actWheelInvert)) {
-    vic_actuatorBackWheel.SetInverted(!vic_actuatorBackWheel.GetInverted());
-  }
-  
   // The solenoids must be in their reverse state
   // in order to attempt to put them forward.
   if(joy_driver.GetRawButtonPressed(drvr_actsDown) && 
@@ -189,25 +198,35 @@ void Robot::TeleopPeriodic() {
     // Make the front actuators go up
     sol_frontActuators.Set(DoubleSolenoid::Value::kReverse);
   }
+  */
 
   // CO-DRIVER CONTROLS //
 
-  // Neither limiter switch is hit, or 
-  // the bottom switch is hit and the arm is moving up, or 
+  // Neither limiter switch is hit, or
+  // the bottom switch is hit and the arm is moving up, or
   // the top switch is hit and the arm is moving down
-  if ((!limiterSwitchBottom.Get() && !limiterSwitchTop.Get()) || 
-      (limiterSwitchBottom.Get() && joy_codriver.GetRawAxis(co_stickY) > 0) || 
-      (limiterSwitchTop.Get() && joy_codriver.GetRawAxis(co_stickY) < 0)) {
+  if ((!limiterSwitchBottom.Get() && !limiterSwitchTop.Get()) ||
+      (limiterSwitchBottom.Get() && joy_codriver.GetRawAxis(co_stickY) > 0) ||
+      (limiterSwitchTop.Get() && joy_codriver.GetRawAxis(co_stickY) < 0))
+  {
     // Move the arm using the joystick
     tln_arm.Set(joy_codriver.GetRawAxis(co_stickY) * armVelMult);
   }
 
-  if(joy_codriver.GetRawButtonPressed(co_cargoPowerToggle)) {
-    // Set the cargo motor off if it's on, or on if it's off.
-    vic_cargoIO.Set(vic_cargoIO.Get() > 0.0 ? 0.0 : 0.5);
+  if (joy_codriver.GetRawButtonPressed(co_cargoPowerToggle))
+  {
+    // Set the cargo motor on
+    vic_cargoIO.Set(0.5);
   }
 
-  if(joy_codriver.GetRawButtonPressed(co_cargoIOToggle)) {
+  if (joy_codriver.GetRawButtonReleased(co_cargoPowerToggle))
+  {
+    // Set the cargo motor off
+    vic_cargoIO.Set(0.0);
+  }
+
+  if (joy_codriver.GetRawButtonPressed(co_cargoIOToggle))
+  {
     // Invert the direction of the cargo motor.
     vic_cargoIO.SetInverted(!vic_cargoIO.GetInverted());
   }
@@ -223,24 +242,27 @@ void Robot::TestInit() {}
 void Robot::TestPeriodic() {}
 
 // User-Defined Functions
-void Robot::UpdateDashboardValues() {
+void Robot::UpdateDashboardValues()
+{
   SmartDashboard::PutBoolean("Cargo on: ", vic_cargoIO.Get() > 0.0);
   SmartDashboard::PutString("Cargo Intake Direction: ", (vic_cargoIO.GetInverted() ? "Out" : "In"));
 
   SmartDashboard::PutBoolean("Bottom pressed: ", limiterSwitchBottom.Get());
   SmartDashboard::PutBoolean("Top pressed: ", limiterSwitchTop.Get());
 
-  SmartDashboard::PutString("Front solenoid value: ",
-  (sol_frontActuators.Get() == DoubleSolenoid::kForward ? "Forward" : 
-  sol_frontActuators.Get() == DoubleSolenoid::kReverse ? "Reverse" : "Off"));
-  SmartDashboard::PutString("Back solenoid value: ", 
-  (sol_backActuators.Get() == DoubleSolenoid::kForward ? "Forward" : 
-  sol_backActuators.Get() == DoubleSolenoid::kReverse ? "Reverse" : "Off"));
+  SmartDashboard::PutNumber("Drivetrain Speed Multiplier: ", wheelVelMult);
 
-  SmartDashboard::PutBoolean("Actuator wheels moving: ", vic_actuatorBackWheel.Get() > 0.0);
-  SmartDashboard::PutString("Actuator wheel direction: ", vic_actuatorBackWheel.GetInverted() ? "Backward" : "Forward");
+  /*
+  SmartDashboard::PutString("Front solenoid value: ",
+                            (sol_frontActuators.Get() == DoubleSolenoid::kForward ? "Forward" : sol_frontActuators.Get() == DoubleSolenoid::kReverse ? "Reverse" : "Off"));
+  SmartDashboard::PutString("Back solenoid value: ",
+                            (sol_backActuators.Get() == DoubleSolenoid::kForward ? "Forward" : sol_backActuators.Get() == DoubleSolenoid::kReverse ? "Reverse" : "Off"));
+                            */
 }
 
 #ifndef RUNNING_FRC_TESTS
-int main() { return StartRobot<Robot>(); }
+int main()
+{
+  return StartRobot<Robot>();
+}
 #endif
